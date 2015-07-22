878ad53f7550806911a006dd40acf8146a8d27cd
jakabadambalazs <jakabadambalazs@gmail.com>
2015-07-22 09:54:15 +0200

    getting vocabulary tree for options

--- src/Mekit/WebformEnhancer.php
+++ src/Mekit/WebformEnhancer.php
@@ -10,21 +10,89 @@ namespace Mekit;
 use Symfony\Component\Yaml\Yaml;
 
 class WebformEnhancer {
-	/** @var  string */
-	private $configurationFilePath;
+	/** @var array  */
+	private $form;
 
-	/** @var array */
-	private $configuration;
-
-	public function __construct() {
-		$this->configurationFilePath = realpath(__DIR__ . '/../../config/efost.yml');
-		$this->configuration = Yaml::parse(file_get_contents($this->configurationFilePath));
+	public function __construct(&$form) {
+		$this->form = &$form;
 	}
 
 	/**
+	 *
+	 */
+	public function enhanceFields() {
+		foreach($this->form["submitted"] as &$field) {
+			if($this->isEnhanceableField($field)) {
+				$this->enhanceField($field);
+			}
+		}
+	}
+
+	/**
+	 * @param array $field
+	 */
+	protected function enhanceField(&$field) {
+		$config = $this->getEnhanceableFieldConfiguration($field);
+
+		unset($field["#value"]);
+		$field["#type"] = $config->type;
+		$field["#empty_option"] = '--- seleziona ---';
+		$field["#options"] = $this->getTaxonomyTermsAsSelectOptions($config);
+	}
+
+	/**
+	 * @param  \stdClass $config
 	 * @return array
 	 */
-	public function getConfiguration() {
-		return $this->configuration["config"];
+	protected function getTaxonomyTermsAsSelectOptions($config)
+	{
+		$answer = [];
+		if(isset($config->vocabulary) && isset($config->level)) {
+			/** @var \stdClass $vocabulary */
+			if($vocabulary = taxonomy_vocabulary_machine_name_load($config->vocabulary)) {
+				$tree = $tree = taxonomy_get_tree($vocabulary->vid, 0, null, true);
+				dpm($tree, "FLAT TREE");
+
+				/*
+				if(count($terms) == 1) {
+					$term = array_pop($terms);
+					if(isset($term->description)) {
+						$answer = $term->description;
+						if($stripHtml) {
+							$answer = strip_tags($answer);
+						}
+					}
+				}*/
+			}
+			else {
+				drupal_set_message("Vocabulary not found by name: " . $config->vocabulary, 'warning');
+			}
+		} else {
+			drupal_set_message("Vocabulary or Level not set in config: " . json_encode($config), 'warning');
+		}
+		return $answer;
+	}
+
+	/**
+	 * @param array $field
+	 * @return bool|\stdClass
+	 */
+	protected function getEnhanceableFieldConfiguration($field) {
+		$config = false;
+		if(isset($field["#value"])) {
+			$config = json_decode($field["#value"]);
+			if(json_last_error() != JSON_ERROR_NONE || !is_object($config)) {
+				$config = false;
+			}
+		}
+		return $config;
+	}
+
+	/**
+	 * @param array $field
+	 * @return bool
+	 */
+	protected function isEnhanceableField($field) {
+		return $this->getEnhanceableFieldConfiguration($field);
 	}
 }
